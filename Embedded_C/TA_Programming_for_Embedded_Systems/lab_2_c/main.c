61180d94008bd0ffd34e8a4aa8ef64030a387e98
Niklas Lantau <niklaslantau@gmail.com>
2022-02-23 23:05:27 +0100

    Added ADC for playtime

--- Embedded_C/TA_Programming_for_Embedded_Systems/lab_2_c/main.c
+++ Embedded_C/TA_Programming_for_Embedded_Systems/lab_2_c/main.c
@@ -1,6 +1,9 @@
 /***************************************************************************
  * Lab 2 - Timers and Counter
  *
+ * Modified to use 5 IR-sensors that toggle 5 LEDs if value of 
+ * IR-sensor is greater than a threshold. Play time with my son.
+ *
  * Created: 2022-02-22
  * Updated: 2022-02-22
  * Author : nlantau
@@ -12,14 +15,16 @@
 #endif /* F_CPU */
 
 /***** Include section ****************************************************/
+#include "lib/adc.h"
 #include "lib/uart.h"
 #include "lib/utils.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <stdlib.h>
-#include <string.h>
+#include <util/delay.h>
 
 /***** Macro Definitions **************************************************/
+#define LAB2 0
+#define PLAYTIME 1
 
 /***** Function prototypes ************************************************/
 void timer0_init(void);
@@ -28,6 +33,7 @@ void led_toggle(void);
 
 /***** Constants **********************************************************/
 static const uint16_t ONE_SEC = 7500;
+static const uint8_t THRESHOLD = 200;
 
 /***** Variables **********************************************************/
 static volatile uint16_t overflow_count = 0;
@@ -35,27 +41,70 @@ static volatile uint16_t overflow_count = 0;
 /***** MAIN ***************************************************************/
 int main(void)
 {
+#if PLAYTIME
+	uint8_t i, j, adc_ports[5] = {0x00, 0x01, 0x02, 0x03, 0x04};
+	uint16_t adc_value[5] = {0};
+	adc_init();
+#endif /* PLAYTIME */
+
+	stdout_init();
+	uart_init();
 	led_init();
+#if LAB2
 	timer0_init();
 	sei();
+	printf("Start\n");
+#endif /* LAB2 */
 
-	for (;;)
-		;
+	for (;;) {
+		/* Fun test
+		 * Use asm
+		 * loop:
+		 * rjump loop
+		 *
+		 */
+		asm volatile ("nop");
+		__no_operation();
+#if PLAYTIME
+		/* IR-array is connected (HW) in the opposite order
+		 * of the LEDs, hence the need for incrementing 
+		 * and decrementing variables
+		 */
+		for (i = 0, j = 6; i < 5; ++i, --j) {
+			adc_read_to_array(adc_ports[i], adc_value);
+			if (adc_value[i] > THRESHOLD) {
+				PORTD ^= (1 << j);
+			}
+			_delay_ms(5);
+		}
 
+#endif /* PLAYTIME */
+	}
 
 	return 0;
 
 } /* End main() */
 
+
 void led_init(void)
 {
-	DDRB |= (1 << PINB1);
-	PORTB &= ~(1 << PINB1);
+	DDRD |= (1 << PIND2) | (1 << PIND3) | (1 << PIND4) | (1 << PIND5) | (1 << PIND6);
+	PORTD &= ~((1 << PIND2) | (1 << PIND3) | (1 << PIND4) | (1 << PIND5) | (1 << PIND6));
+
+
 } /* End led_init() */
 
 void led_toggle(void)
 {
-	PORTB ^= (1 << PINB1);
+#if LAB2
+	int i;
+	printf("> Toggling LED: %s\n", PINB & (1 << PINB1) ? "Off" : "On");
+	for (i = 0; i < 40; ++i) {
+		printf("%c", uart_getc());
+	}
+	printf("\n");
+#endif
+	PORTD ^= (1 << PIND2) | (1 << PIND3) | (1 << PIND4) | (1 << PIND5) | (1 << PIND6);
 
 } /* End led_toggle() */
 
