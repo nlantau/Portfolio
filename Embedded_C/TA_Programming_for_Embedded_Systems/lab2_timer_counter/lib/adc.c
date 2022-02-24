04deaeaa5dbedb2d6f0b945525cb0d6a13026168
Niklas Lantau <niklaslantau@gmail.com>
2022-02-24 15:30:38 +0100

    CTC Timer

---
+++ Embedded_C/TA_Programming_for_Embedded_Systems/lab2_timer_counter/lib/adc.c
@@ -0,0 +1,67 @@
+/***************************************************************************
+ * ADC Source File
+ *
+ * Created: 2022-02-19
+ * Updated: 2022-02-19
+ * Author: nlantau
+ **************************************************************************/
+
+/***************************************************************************
+ *  Info: See adc.h
+ *
+ **************************************************************************/
+
+/***** Include section ****************************************************/
+#include "adc.h"
+
+
+/***** Functions **********************************************************/
+uint8_t adc_init(void)
+{
+	/* 5V reference */
+	ADMUX |= (1 << REFS0);
+
+	/* Prescaler 128 */
+	ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2);
+
+	/* Enable ADC */
+	ADCSRA |= (1 << ADEN);
+
+	return SUCCESS;
+
+} /* End adc_init() */
+
+uint8_t adc_read_to_array(uint8_t adc_channel, uint16_t *adc_arr, uint8_t arr_index)
+{
+	/* Select channel with safety mask */
+	ADMUX = (ADMUX & 0xF0) | (adc_channel & 0x0F);
+
+	/* Start conversion */
+	ADCSRA |= (1 << ADSC);
+
+	loop_until_bit_is_clear(ADCSRA, ADSC);
+
+	if (ADC >= 0)
+		adc_arr[arr_index] = ADC;
+	else
+		adc_arr[arr_index] = 0;
+
+	return SUCCESS;
+
+} /* End adc_read_to_array() */
+
+uint16_t adc_read(uint8_t adc_channel)
+{
+	/* Select channel with safety mask */
+	ADMUX = (ADMUX & 0xF0) | (adc_channel & 0x0F);
+
+	/* Start conversion */
+	ADCSRA |= (1 << ADSC);
+
+	loop_until_bit_is_clear(ADCSRA, ADSC);
+
+	return ADC;
+
+} /* End adc_read() */
+
+/* End adc.c */
---
+++ Embedded_C/TA_Programming_for_Embedded_Systems/lab2_timer_counter/lib/uart.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+ * UART Header File
+ *
+ * Created: 2022-02-19
+ * Updated: 2022-02-19
+ * Author: nlantau
+ **************************************************************************/
+
+/***************************************************************************
+ *  Info: Very basic for now. Using FDEV_SETUP_STREAM to set stdout to
+ *        UART, which makes it possible to use printf() from stdio.h to
+ *        send FILE stream of characters to a PC serial monitor.
+ *  TODO: Implement 'uart_getc()' 
+ *
+ **************************************************************************/
+
+/***** Include guard ******************************************************/
+#ifndef UART_S_H_
+#define UART_S_H_
+
+#ifndef F_CPU
+#define F_CPU        16000000UL
+#endif /* F_CPU */
+
+/***** Include section ****************************************************/
+#include <stdio.h>
+#include <avr/io.h>
+
+/***** Macro Definitions **************************************************/
+#define BAUD 9600
+#define MYUBRR F_CPU/16/BAUD-1
+#define SUCCESS 1
+
+/***** Function prototypes ************************************************/
+
+extern uint8_t uart_init(void);
+
+/* Must return 'int' according to the definition of 
+ * FDEV_SETUP_STREAM() in stdio.h */
+extern int uart_putchar_stream(char c, FILE *stream);
+extern uint8_t stdout_init(void);
+extern void uart_putc(uint8_t data);
+extern uint8_t uart_getc(void);
+extern void uart_puts(const char *s);
+
+#endif /* UART_S_H_ */
+
