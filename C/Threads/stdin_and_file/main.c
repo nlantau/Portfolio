487acc241c5a07d91894ab060beca10c24ff4120
Niklas Lantau <niklaslantau@gmail.com>
2022-03-10 00:01:45 +0100

    update

--- C/Threads/stdin_and_file/main.c
+++ C/Threads/stdin_and_file/main.c
@@ -31,26 +31,46 @@ e_return write_time_file(void *arg);
 
 /***** Constants **********************************************************/
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
 
 /***** Variables **********************************************************/
 uint8_t counter = 20;
+struct data
+{
+    uint8_t iret1;
+    uint8_t iret2;
+} ret_val;
+
+struct user_data
+{
+    char *name;
+    uint8_t counter;
+};
+
 
 /***** MAIN ***************************************************************/
 
 int main(int argc, char *argv[])
 {
-    pthread_t thread1, thread2;
-    uint8_t iret1, iret2;
+    struct user_data *user = malloc(sizeof(struct user_data));
+    user->name = malloc(sizeof(char) * 100);
+    strcpy(user->name, "unnamed");
 
-    iret1 = pthread_create(&thread1, NULL, (void *)write_time_file, NULL);
-    iret2 = pthread_create(&thread2, NULL, (void *)loopback_stdin, NULL);
+    pthread_t thread1, thread2;
+
+    ret_val.iret1 = pthread_create(&thread1, NULL, (void *)write_time_file, (void *)user);
+    ret_val.iret2 = pthread_create(&thread2, NULL, (void *)loopback_stdin, (void *)user);
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
 
     fprintf(stdout,
             "> iret1: %d, iret2: %d, counter: %d\n",
-            iret1, iret2, counter);
+            ret_val.iret1, ret_val.iret2, counter);
+
+
+    free(user->name); user->name = NULL;
+    free(user); user = NULL;
 
     return 0;
 
@@ -78,13 +98,18 @@ e_return loopback_stdin(void *arg)
 
         fprintf(stdout, ">> %s\n", buf);
 
+        /* Mutex: counter */
         pthread_mutex_lock(&mutex1);
         cont = counter;
-
         fprintf(stdout, ">> Counter: %d\n", counter);
-
         pthread_mutex_unlock(&mutex1);
 
+        /* Mutex: user_data structure*/
+        pthread_mutex_lock(&mutex2);
+        strcpy(((struct user_data*)arg)->name, buf);
+        fprintf(stdout, ">> %s\n", ((struct user_data*)arg)->name);
+        ((struct user_data*)arg)->counter = counter;
+        pthread_mutex_unlock(&mutex2);
     }
 
     return RET_OK;
@@ -96,15 +121,16 @@ e_return write_time_file(void *arg)
     int8_t cont = 1;
     while (cont >= 0) {
 
+        /* Mutex: counter */
         pthread_mutex_lock(&mutex1);
         cont = counter--;
         pthread_mutex_unlock(&mutex1);
 
-        FILE *out;
+        FILE *fp;
 
-        if (!(out = fopen("output.txt", "a"))) {
+        if (!(fp = fopen("output.txt", "a"))) {
             fprintf(stderr, "> Could not open file\n");
-            fclose(out);
+            fclose(fp);
             return RET_ERROR;
         }
 
@@ -114,8 +140,20 @@ e_return write_time_file(void *arg)
 
         strftime(s, sizeof(s), "%c", tm);
 
-        fprintf(out, "%s\n", s);
-        fclose(out);
+        fprintf(fp, "%s,", s);
+
+        /* Mutex: user_data structure */
+        pthread_mutex_lock(&mutex2);
+        fprintf(fp, "%s, %d\n",
+                ((struct user_data*)arg)->name,
+                ((struct user_data*)arg)->counter);
+
+        /* Reset struct fields*/
+        strcpy(((struct user_data*)arg)->name, "-");
+        ((struct user_data*)arg)->counter = counter;
+        pthread_mutex_unlock(&mutex2);
+
+        fclose(fp);
 
         sleep(1);
     }
